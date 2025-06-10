749c3c447a02f9962a458066c0efd580a407db9f
shravanip14 <patilshravani310@gmail.com>
2025-06-10 22:09:18 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main ()
+{
+    int num;
+    printf("enter the number\t");
+    scanf("%d",&num);
+    switch(num)
+    {
+        case 1:
+        printf("MONDAY");
+        break;
+        case 2:
+        printf("TUESDAY");
+        break;
+        case 3:
+        printf("WENESDAY");
+        break;
+        case 4:
+        printf("THURSDAY");
+        break;
+        case 5:
+        printf("FRIDAY");
+        break;
+        case 6:
+        printf("SATURDAY");
+        break;
+        case 7:
+        printf("SUNDAY");
+        break;
+        default:
+        printf("incorrect");
+    }
+    return 0;
+}
