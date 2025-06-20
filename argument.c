0a51efa9128e7bdbd1bfc2b9cf482a0ecfb0b093
shravanip14 <patilshravani310@gmail.com>
2025-06-20 19:42:52 +0530

    Add files via upload

---
+++ argument.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+ int add(int c,int d)
+ {
+    int sum=c+d; 
+    printf("sum is %d\n",sum);
+
+ }
+    
+
+int main()
+{
+    int num1=4,num2=8;
+    add(num1,num2);
+    return 0;
+}
+
